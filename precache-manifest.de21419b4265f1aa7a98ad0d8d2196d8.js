1182d6d70e4c32516fd1617e061de2f223784f51
EzequielCaste <ezequiel.caste@gmail.com>
2020-10-06 22:22:10 -0300

    Updates

---
+++ precache-manifest.de21419b4265f1aa7a98ad0d8d2196d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37bb3944cc176d88d7e315e6db4eb72",
+    "url": "/react-social-cards/index.html"
+  },
+  {
+    "revision": "bac08295871870a1a5ed",
+    "url": "/react-social-cards/static/css/main.310d7d28.chunk.css"
+  },
+  {
+    "revision": "7969e83f3b178eb6143e",
+    "url": "/react-social-cards/static/js/2.b06abf86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-social-cards/static/js/2.b06abf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac08295871870a1a5ed",
+    "url": "/react-social-cards/static/js/main.93ff8f14.chunk.js"
+  },
+  {
+    "revision": "28c25c80d4eed4db2c2b",
+    "url": "/react-social-cards/static/js/runtime-main.7ce8400a.js"
+  }
+]);
